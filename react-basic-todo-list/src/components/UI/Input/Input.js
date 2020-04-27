39a4c2cb28a48968be784f68b5a0baba3bb359ca
Gonzalo <gonzalo.s.otheguy@gmail.com>
2020-04-27 14:42:17 -0300

    Bug fix

--- react-basic-todo-list/src/components/UI/Input/Input.js
+++ react-basic-todo-list/src/components/UI/Input/Input.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './Input.css';
 
+const handleFocus = (event) => {
+    event.target.value = "";
+};
+
 const input = (props) => (
-    <input className="Input" value={props.value} onChange={props.onChange} placeholder="Add a task" />
+    <input className="Input" value={props.value || ''} onChange={props.onChange} onFocus={handleFocus} placeholder="Add a task" />
 );
 
 export default input;
--- react-basic-todo-list/src/containers/TodoList/TodoList.js
+++ react-basic-todo-list/src/containers/TodoList/TodoList.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Button from '../../components/UI/Button/Button';
-import SimpleList from '../../components/SimpleList/SimpleList';
 import Input from '../../components/UI/Input/Input';
 import './TodoList.css';
 
+const CustomCheckbox = withStyles({
+    root: {
+      color: 'rgba(0, 0, 0, 0.54)',
+      '&$checked': {
+        color: '#00bcd4',
+      },
+    },
+    checked: { },
+  })(props => <Checkbox color="default" {...props} />);
+
 class TodoList extends Component {
     state = {
-        term: '',
         items: []
     }
 
@@ -17,11 +35,22 @@ class TodoList extends Component {
     }
 
     addItemHandler = (event) => {
-        event.preventDefault()
+        event.preventDefault();
         this.setState({
-          term: '',
-          items: [...this.state.items, this.state.term]
+            items: [...this.state.items, {term: this.state.term, checked: false}]
         });
+    }
+
+    deleteItemHandler = (index) => {
+        const items = [...this.state.items];
+        items.splice(index, 1);
+        this.setState({items: items});
+    }
+
+    checkedItemHandler = (index) => {
+        const items = [...this.state.items];
+        items[index] = { term: items[index].term, checked: items[index].checked ? false : true };
+        this.setState({ items: items });
     }
 
     render() {
@@ -29,7 +58,26 @@ class TodoList extends Component {
             <Auxiliary>
                 <Input value={this.state.term} onChange={this.onChange} />
                 <Button clicked={this.addItemHandler} />
-                <SimpleList items={this.state.items} /> 
+                <div className="Root">
+                    <Grid item xs={12} md={6} className="Grid" style={ this.state.items.length <= 0 ? { display: "none" }: {}}>
+                        <Typography variant="h6"></Typography>
+                        <div>
+                        <List>
+                        { this.state.items.map((item, index) => 
+                            <ListItem key={index}>
+                                <ListItemIcon>
+                                    <CustomCheckbox size="small" onChange={this.checkedItemHandler.bind(this, index)} />
+                                </ListItemIcon>
+                                <ListItemText style={ item.checked ? { color: "#989898", textDecoration: 'line-through' } : {}} primary={item.term} />
+                                <IconButton edge="end" aria-label="delete">
+                                    <DeleteIcon className="DeleteIcon" onClick={this.deleteItemHandler.bind(this, index)} />
+                                </IconButton>
+                            </ListItem>
+                        )}
+                        </List>
+                        </div>
+                    </Grid>
+                </div>
             </Auxiliary>
         );
     }
